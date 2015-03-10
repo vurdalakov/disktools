c4bc43ad986d807be150c54167fda84ea6e119fc
vurdalakov <vurdalakov@gmail.com>
2015-03-10 07:29:41 +0200

    diskinfo: using clause

--- src/diskinfo/Application.cs
+++ src/diskinfo/Application.cs
@@ -16,18 +16,20 @@
                 {
                     Console.WriteLine("\n--- Physical disk {0}\n", diskNumber);
 
-                    var disk = new PhysicalDisk(diskNumber, true);
-                    disk.ReadDiskInformation();
+                    using (var disk = new PhysicalDisk(diskNumber, true))
+                    {
+                        disk.ReadDiskInformation();
 
-                    Console.WriteLine("DISK_GEOMETRY:");
-                    Console.WriteLine("Cylinders:\t\t{0:N0}", disk.DiskGeometry.Cylinders);
-                    Console.WriteLine("MediaType:\t\t{0}", disk.DiskGeometry.MediaType);
-                    Console.WriteLine("TracksPerCylinder:\t{0:N0}", disk.DiskGeometry.TracksPerCylinder);
-                    Console.WriteLine("SectorsPerTrack:\t{0:N0}", disk.DiskGeometry.SectorsPerTrack);
-                    Console.WriteLine("BytesPerSector:\t\t{0:N0}", disk.DiskGeometry.BytesPerSector);
+                        Console.WriteLine("DISK_GEOMETRY:");
+                        Console.WriteLine("Cylinders:\t\t{0:N0}", disk.DiskGeometry.Cylinders);
+                        Console.WriteLine("MediaType:\t\t{0}", disk.DiskGeometry.MediaType);
+                        Console.WriteLine("TracksPerCylinder:\t{0:N0}", disk.DiskGeometry.TracksPerCylinder);
+                        Console.WriteLine("SectorsPerTrack:\t{0:N0}", disk.DiskGeometry.SectorsPerTrack);
+                        Console.WriteLine("BytesPerSector:\t\t{0:N0}", disk.DiskGeometry.BytesPerSector);
 
-                    var diskSize = disk.DiskGeometry.Cylinders * disk.DiskGeometry.TracksPerCylinder * disk.DiskGeometry.SectorsPerTrack * disk.DiskGeometry.BytesPerSector;
-                    Console.WriteLine("\nDisk size:\t\t{0:N0} bytes", diskSize);
+                        var diskSize = disk.DiskGeometry.Cylinders * disk.DiskGeometry.TracksPerCylinder * disk.DiskGeometry.SectorsPerTrack * disk.DiskGeometry.BytesPerSector;
+                        Console.WriteLine("\nDisk size:\t\t{0:N0} bytes", diskSize);
+                    }
                 }
 
                 Console.WriteLine("\n=== Logical disks");
@@ -38,18 +40,20 @@
                 {
                     Console.WriteLine("\n--- Logical disk {0}:\n", diskChar);
 
-                    var disk = new LogicalDisk(diskChar, true); // TODO: using
-                    disk.ReadDiskInformation();
+                    using (var disk = new LogicalDisk(diskChar, true))
+                    {
+                        disk.ReadDiskInformation();
 
-                    Console.WriteLine("PARTITION_INFORMATION:");
-                    Console.WriteLine("StartingOffset:\t\t{0:N0}", disk.PartitionInformation.StartingOffset);
-                    Console.WriteLine("PartitionLength:\t{0:N0}", disk.PartitionInformation.PartitionLength);
-                    Console.WriteLine("HiddenSectors:\t\t{0:N0}", disk.PartitionInformation.HiddenSectors);
-                    Console.WriteLine("PartitionNumber:\t{0}", disk.PartitionInformation.PartitionNumber);
-                    Console.WriteLine("PartitionType:\t\t0x{0:X2} ({1})", disk.PartitionInformation.PartitionType, LogicalDisk.GetPartitionTypeString(disk.PartitionInformation.PartitionType));
-                    Console.WriteLine("BootIndicator:\t\t{0}", disk.PartitionInformation.BootIndicator);
-                    Console.WriteLine("RecognizedPartition:\t{0}", disk.PartitionInformation.RecognizedPartition);
-                    Console.WriteLine("RewritePartition:\t{0}", disk.PartitionInformation.RewritePartition);
+                        Console.WriteLine("PARTITION_INFORMATION:");
+                        Console.WriteLine("StartingOffset:\t\t{0:N0}", disk.PartitionInformation.StartingOffset);
+                        Console.WriteLine("PartitionLength:\t{0:N0}", disk.PartitionInformation.PartitionLength);
+                        Console.WriteLine("HiddenSectors:\t\t{0:N0}", disk.PartitionInformation.HiddenSectors);
+                        Console.WriteLine("PartitionNumber:\t{0}", disk.PartitionInformation.PartitionNumber);
+                        Console.WriteLine("PartitionType:\t\t0x{0:X2} ({1})", disk.PartitionInformation.PartitionType, LogicalDisk.GetPartitionTypeString(disk.PartitionInformation.PartitionType));
+                        Console.WriteLine("BootIndicator:\t\t{0}", disk.PartitionInformation.BootIndicator);
+                        Console.WriteLine("RecognizedPartition:\t{0}", disk.PartitionInformation.RecognizedPartition);
+                        Console.WriteLine("RewritePartition:\t{0}", disk.PartitionInformation.RewritePartition);
+                    }
                 }
 
                 return 0;
